f6c34d2faf5c6174896512abbca30e07f2fcd072
kienphan <senior1206@gmail.com>
2016-08-07 21:23:21 +0900

    fix bug cors

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   #
   get 'maze', controller: :maze, action: :index, as: :maze
 
+  match '*any' => 'application#options', :via => [:options]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
